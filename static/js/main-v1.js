c804a40194eb84c98a499d88d571594c2a0e7293
jneilliii <jneilliii+github@gmail.com>
2020-12-03 22:29:39 -0500

    move name mapping to separate file

--- static/js/main-v1.js
+++ static/js/main-v1.js
@@ -1,48 +1,5 @@
 var date = new Date()
 var PATH_TO_STATS = "data/stats.json?t=" + date.getTime()
-var names = {
-    stlviewer: "STL Viewer",
-    displayprogress: "DisplayProgress",
-    m117navbar: "OctoPrint-M117NavBar",
-    m117popup: "OctoPrint-M117PopUp",
-    bltouch: "OctoPrint-BLTouch",
-    youtubelive: "YouTube Live",
-    custombackground: "OctoPrint-CustomBackground",
-    tplinksmartplug: "OctoPrint-TPLinkSmartplug",
-    tasmota: "OctoPrint-Tasmota",
-    floatingnavbar: "OctoPrint-FloatingNavbar",
-    mqttpublish: "OctoPrint-MQTTPublish",
-    tasmota_mqtt: "OctoPrint-TasmotaMQTT",
-    m117speechsynthesis: "OctoPrint-M117SpeechSynthesis",
-    taborder: "OctoPrint-TabOrder",
-    bedlevelvisualizer: "Bed Level Visualizer",
-    iponconnect: "OctoPrint-ipOnConnect",
-    sidebartempgraph: "OctoPrint-SideBarTempGraph",
-    domoticz: "OctoPrint-Domoticz",
-    bedlevelingwizard: "OctoPrint-BedLevelingWizard",
-    statefulsidebar: "Stateful Sidebar",
-    rtmpstreamer: "OctoPrint-RTMPStreamer",
-    wemoswitch: "OctoPrint-WemoSwitch",
-    active_filters_extended: "Active Filters Extended",
-    dragon_order: "OctoPrint-DragonOrder",
-    myminifactory: "OctoPrint-MyMiniFactory",
-    ultimakerformatpackage: "Cura Thumbnails",
-    terminalcommandsextended: "Terminal Commands Extended",
-    python3plugincompatibilitycheck: "Python 3 Check",
-    autoterminalinput: "Auto Terminal Input",
-    prusaslicerthumbnails: "PrusaSlicer Thumbnails",
-    easyservo: "Easy Servo",
-    multilineterminal: "Multi Line Terminal",
-    mqttsubscribe: "MQTT Subscribe",
-    m300player: "M300 Player",
-    stickypad: "Sticky Pad",
-    webcam_iframe: "Webcam Iframe",
-    widescreen: "OctoPrint-WideScreen",
-    consolidate_temp_control: "Consolidate Temp Control",
-    arducamfocus: "ArduCamFocus",
-    consolidatedtabs: "Consolidated Tabs",
-    backupscheduler: "Backup Scheduler"
-}
 
 function ajaxGet(path, callback) {
     var xhttp = new XMLHttpRequest();
