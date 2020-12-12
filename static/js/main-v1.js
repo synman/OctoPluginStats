431f7def7b20245db79caab8d790b347e329da7b
jneilliii <jneilliii+github@gmail.com>
2020-12-12 00:06:15 -0500

    move get document inside scroll function

--- static/js/main-v1.js
+++ static/js/main-v1.js
@@ -129,13 +129,12 @@ function createHistoryChart(data, element, name){
 
 }
 
-//Get the button:
-const mybutton = document.getElementById("top_btn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  //Get the button:
+  var mybutton = document.getElementById("top_btn");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
